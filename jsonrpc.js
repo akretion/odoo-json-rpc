9bb750e56c173a21382f0dbf6b372963defe38da
Adrien Chaussende <chaussende.adrien@gmail.com>
2014-12-18 17:25:00 +0100

    [IMP] Methods for search/read and call other methods on Odoo database

--- jsonrpc.js
+++ jsonrpc.js
@@ -2,7 +2,27 @@ if(typeof ODOORPC != 'object') {
     ODOORPC = {};
 }
 
+
 (function() {
+
+    ODOORPC.requestCounts = 0;
+
+    ODOORPC.sendRequest = function(url, params) {
+        ODOORPC.requestCounts += 1;
+        var json_data = {
+            jsonrpc: '2.0',
+            method: 'call',
+            params: params,
+        };
+        var request = {
+            'type' : 'POST',
+            'url' : url,
+            'data' : JSON.stringify(json_data),
+            'contentType' : 'application/json',
+            'id' : 'r' + ODOORPC.requestCounts
+        };
+        return $.ajax(request);
+    };
 
     ODOORPC.login = function(db, login, password) {
         var params = {
@@ -13,20 +33,22 @@ if(typeof ODOORPC != 'object') {
         return ODOORPC.sendRequest('/web/session/authenticate', params);
     };
 
-    ODOORPC.sendRequest = function(url, params) {
-      var json_data = {
-        'jsonrpc': '2.0',
-        'method': 'call',
-        'params': params,
-      };
-      var request = {
-        'type' : 'POST',
-        'url' : url,
-        'data' : JSON.stringify(json_data),
-        'contentType' : 'application/json',
-        'id' : 'r1'
-      };
+    ODOORPC.searchRead = function(model, domain, fields) {
+        params = {
+            model: model,
+            domain: domain,
+            fields: fields,
+        }
+        return ODOORPC.sendRequest('/web/dataset/search_read', params);
+    }
 
-      return $.ajax(request);
-    };
+    ODOORPC.call = function(model, method, args, kwargs) {
+        params = {
+            model: model,
+            method: method,
+            args: args,
+            kwargs: kwargs
+        };
+        return ODOORPC.sendRequest('/web/dataset/call_kw', params);
+    }
 }());
